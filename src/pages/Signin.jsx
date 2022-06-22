fac87e7142fc851e7cf76eaf5785b4ef0d6df732
nehatkkhan786 <33215298+nehatkkhan786@users.noreply.github.com>
2022-06-22 16:49:06 +0530

    added hte backend url

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -86,7 +86,7 @@ const Signin = () => {
   
   const onSuccssHandler = async (response)=>{
     console.log(response.accessToken)
-    const {data} = await axios.post('http://127.0.0.1:8000/auth/google_login/', {'access_token':response.accessToken}, {
+    const {data} = await axios.post('https://api.manxho.co.in/api/auth/google_login/', {'access_token':response.accessToken}, {
       headers:{
         'content-type': 'application/json'
       }
