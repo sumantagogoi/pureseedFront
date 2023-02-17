5a30e37988168605ddaa4809c3f9ecd59770d765
Sumanta Gogoi <sumanta@xynocast.com>
2023-02-18 01:48:12 +0530

    no message

--- src/components/PaymentDialog.jsx
+++ src/components/PaymentDialog.jsx
@@ -24,20 +24,36 @@ const PaymentDialog = () => {
   console.log(orderId)
   console.log(orderTotalAmoount)
 
+  const [showBox, setShowBox] = React.useState(false);
+
+  const UseUPI = async () => {
+    try {
+      const response = await fetch("https://api.manxho.co.in/api/upi_order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ order_id: 104 })
+      });
+      const data = await response.json();
+      console.log(data); // Do something with the response data
+    } catch (error) {
+      console.error(error);
+    }
+    setShowBox(true);
+  }
+
   const redirecttoPayment = async () => {
-    fetch("https://api.manxho.co.in/api/upi_order", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ order_id: 104 })
-    });
+
+
     window.location.href = `upi://pay?pa=manxho@icici&pn=Manxho&cu=INR&am=${orderTotalAmoount}&tn=order-${orderId}`
   }
+
   const generateUPIqrcode = () => {
     let uplink = `upi://pay?pa=manxho@icici&pn=Manxho&cu=INR&am=${orderTotalAmoount}&tn=order-${orderId}`
     setQrValue(uplink)
   }
+
   const paymentHandler = () => {
     navigate(`/order_detail/${orderId}`)
     toast.success('Thanks For ordering!')
@@ -58,21 +74,26 @@ const PaymentDialog = () => {
     >
       <DialogTitle>Select Payment Method:</DialogTitle>
       <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={() => redirecttoPayment()} size='small' variant='contained' endIcon={<Box component='img' src={UPI} sx={{ width: 40, height: 40, borderRadius: 10, }} />} sx={{ color: '#ffffff', backgroundColor: 'black', ':hover': { bgcolor: 'brown' } }}>Pay With UPI</Button>
+        <Button onClick={() => UseUPI()} size='small' variant='contained' endIcon={<Box component='img' src={UPI} sx={{ width: 40, height: 40, borderRadius: 10, }} />} sx={{ color: '#ffffff', backgroundColor: 'black', ':hover': { bgcolor: 'brown' } }}>Pay With UPI</Button>
         <Button size='small' variant='contained' disabled>Pay With Razorpay</Button>
       </DialogActions>
-      <Typography sx={{ mt: 2, textAlign: 'center' }}>Or Scan The code with your UPI App</Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center', margin: 4 }}>
-        <QRCode value={`upi://pay?pa=manxho@icici&pn=Manxho&cu=INR&am=${orderTotalAmoount}&tn=order-${orderId}`} />
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', }}>
-        <Button size='large' variant='contained' endIcon={<PriceCheckIcon />} onClick={() => paymentHandler()} >Paid</Button>
-      </Box>
-      <Box sx={{ margin: 2 }}>
-        <Typography>Note:</Typography>
-        <Typography>1: Kindly Wait for sometime after making the payment as order confirmation will be done manually.</Typography>
-        <Typography>2: Kindly press the button 'PAID' after making the payment</Typography>
-      </Box>
+      {
+        showBox && <Box><Typography onClick={() => redirecttoPayment()} sx={{ mt: 2, textAlign: 'center' }}>Click to open UPI APP</Typography>
+          <Typography sx={{ mt: 2, textAlign: 'center' }}>Or Scan The code with your UPI App</Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', margin: 4 }}>
+            <QRCode value={`upi://pay?pa=manxho@icici&pn=Manxho&cu=INR&am=${orderTotalAmoount}&tn=order-${orderId}`} />
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', }}>
+            <Button size='large' variant='contained' endIcon={<PriceCheckIcon />} onClick={() => paymentHandler()} >Paid</Button>
+          </Box>
+          <Box sx={{ margin: 2 }}>
+            <Typography>Note:</Typography>
+            <Typography>1: Kindly Wait for sometime after making the payment as order confirmation will be done manually.</Typography>
+            <Typography>2: Kindly press the button 'PAID' after making the payment</Typography>
+          </Box>
+        </Box>
+      }
+
     </Dialog>
   )
 }
