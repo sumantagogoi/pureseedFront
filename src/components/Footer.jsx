98ab3c1c19531136a16605420816c678ac9d2e1e
Sentinelhz <sentinelhz344@gmail.com>
2023-03-21 05:09:45 +0530

    no message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,23 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import fssai from '../assets/Images/fssai.jpg'
+
 const Footer = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
   return (
-    <Box sx={{display:'flex', justifyContent:'center',  gap:'20px', mt:5, pb:2, width:'100%'}}>
-        <Typography onClick={()=>navigate('')} sx={{cursor:'pointer', textDecoration:'none'}}>Refunds And Cancellation</Typography>
-        <Typography onClick={()=>navigate('')} sx={{cursor:'pointer'}}>Terms And Condition</Typography>
-        <Typography onClick={()=>navigate('')} sx={{cursor:'pointer'}}>Privacy Policy</Typography>
-    </Box>
+    <>
+      <Box sx={{ display: 'flex', textAlign:'center', justifyContent: 'center', mt: 7, width: '100%' }}>
+        <img src={fssai} alt="" />
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: '20px', mt: 2 , pb: 2, width: '100%' }}>
+        <Typography onClick={() => navigate('')} sx={{ cursor: 'pointer', textDecoration: 'none' }}>Refunds And Cancellation</Typography>
+        <Typography onClick={() => navigate('')} sx={{ cursor: 'pointer' }}>Terms And Condition</Typography>
+        <Typography onClick={() => navigate('')} sx={{ cursor: 'pointer' }}>Privacy Policy</Typography>
+      </Box>
+    </>
+
   )
 }
 
