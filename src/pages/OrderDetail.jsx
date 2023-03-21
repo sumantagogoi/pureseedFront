9865bf4eb83b79429b81391ea3d86bff0ebe9676
Sentinelhz <sentinelhz344@gmail.com>
2023-03-21 08:21:30 +0530

    no message

--- src/pages/OrderDetail.jsx
+++ src/pages/OrderDetail.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Container, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material'
+import { Avatar, Box, Button, capitalize, Container, Divider, Grid, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material'
 import React, { useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { useParams } from 'react-router-dom'
@@ -12,91 +12,93 @@ import axios from 'axios'
 
 const OrderDetail = () => {
 
-    const {allOrdersByUser} = useContext(ProductContext)
-    const {userLoginDetails} = useContext(AuthenticationContext)
-    const [order, setOrder] = useState([])
-   
-    const params = useParams()
+  const { allOrdersByUser } = useContext(ProductContext)
+  const { userLoginDetails } = useContext(AuthenticationContext)
+  const [order, setOrder] = useState([])
 
-    const getOrderById = async ()=>{
-        const {data} = await axios.get(`https://api.manxho.co.in/api/users/order/${params.orderId}`,{
-            headers:{
-                'content-type':'application/json',
-                'Authorization': `Bearer ${userLoginDetails?.access_token}`
-            }
-        });
-        setOrder(data)
+  const params = useParams()
 
-    }
+  const getOrderById = async () => {
+    const { data } = await axios.get(`https://api.manxho.co.in/api/users/order/${params.orderId}`, {
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${userLoginDetails?.access_token}`
+      }
+    });
+    setOrder(data)
+  }
 
-    useEffect(()=>{
-        getOrderById()
-    }, [])
-    
-   
+  useEffect(() => {
+    getOrderById()
+  }, [])
+
+
   return (
     <motion.div
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-    <Container sx={{minHeight:'100vh',pt:13, pb:4}}>
-     
-     
-      <Typography align='center' variant='h4'>Order-{params.orderId}</Typography>
-     
-     
-       
-       
+      <Container sx={{ minHeight: '100vh', pt: 13, pb: 4 }}>
+
+        <Typography align='center' variant='h4'>Order-{params.orderId}</Typography>
+
         <List>
-           {order?.orderItems?.map((item)=>(
-                <ListItem key={item?._id}>
-                <ListItemAvatar>
-                 <Avatar 
-                 src={`https://api.manxho.co.in${item?.image}`}
-                 sx={{width:56, height:56}}
-                 />
-                </ListItemAvatar>
-                <ListItemText sx={{pl:2}}>{item?.name}</ListItemText>
-                <Typography variant='body2'>{item?.qty} x {item?.price} &#8377; {Number(item?.price * item?.qty).toFixed(0)}</Typography>
-              </ListItem>
-           ))}
-           <Divider/>   
-           <ListItem>
-             <ListItemText>Shipping:</ListItemText>
-             <Typography variant='subtitle1'> &#8377;{order?.shippingPrice}</Typography>
-           </ListItem>
+          {order?.orderItems?.map((item) => (
+            <ListItem key={item?._id}>
+              <ListItemAvatar>
+                <Avatar
+                  src={`https://api.manxho.co.in${item?.image}`}
+                  sx={{ width: 56, height: 56 }}
+                />
+              </ListItemAvatar>
+              <ListItemText sx={{ pl: 2 }}>{item?.name}</ListItemText>
+              <Typography variant='body2'>{item?.qty} x {item?.price} &#8377; {Number(item?.price * item?.qty).toFixed(0)}</Typography>
+            </ListItem>
+          ))}
+          <Divider />
+          <ListItem>
+            <ListItemText>Shipping:</ListItemText>
+            <Typography variant='subtitle1'> &#8377;{order?.shippingPrice}</Typography>
+          </ListItem>
 
-           {order?.coupon !== ''}{
-                 <ListItem>
-                 <ListItemText>Coupon:</ListItemText>
-                 <Typography variant='subtitle1'>{order?.coupon}</Typography>
-               </ListItem>
-           }
+          {order?.coupon !== ''}{
             <ListItem>
-             <ListItemText>Total:</ListItemText>
-             <Typography variant='subtitle1'> &#8377;{order?.totalPrice}</Typography>
-           </ListItem>
+              <ListItemText>Coupon:</ListItemText>
+              <Typography variant='subtitle1'>{order?.coupon}</Typography>
+            </ListItem>
+          }
+          <ListItem>
+            <ListItemText>Total:</ListItemText>
+            <Typography variant='subtitle1'> &#8377;{order?.totalPrice}</Typography>
+          </ListItem>
 
-           {order.isPaid ? (
-                <ListItem>
-                <ListItemText>Payment Status:</ListItemText>
-                <Typography variant='subtitle1'> Paid</Typography>
-              </ListItem>
-           ): (<ListItem>
+          {order.isPaid ? (
+            <ListItem>
+              <ListItemText>Payment Status:</ListItemText>
+              <Typography variant='subtitle1'> Paid</Typography>
+            </ListItem>
+          ) : (<ListItem>
             <ListItemText>Payment Status:</ListItemText>
             <Typography variant='subtitle1'>Awaiting Payment Confirmation</Typography>
           </ListItem>)}
 
           {order.isDelivered ? (
-                <ListItem>
-                <ListItemText>Delivery Status:</ListItemText>
-                <Typography variant='subtitle1'>Delivered</Typography>
-              </ListItem>
-           ): (<ListItem>
-            <ListItemText>Delivery Status:</ListItemText>
-            <Typography variant='subtitle1'>{order?.status}</Typography>
+            <ListItem>
+              <ListItemText>Order Status:</ListItemText>
+              <Typography variant='subtitle1'>Delivered</Typography>
+            </ListItem>
+          ) : (<ListItem>
+            <ListItemText>Order Status:</ListItemText>
+            <Typography variant='subtitle1' sx={{ textTransform: "capitalize" }}>{order?.status}</Typography>
           </ListItem>)}
+
+          {order?.status == "dispatched" ? (
+            <ListItem>
+              <ListItemText>Courier details:</ListItemText>
+              <Typography variant='subtitle1'>{order?.courierInfo}</Typography>
+            </ListItem>
+          ) : (<></>)}
 
         </List>
         <Grid container spacing={2}>
@@ -104,14 +106,14 @@ const OrderDetail = () => {
             <Typography variant='h5' gutterBottom>Shipping Details</Typography>
             <Typography>{userLoginDetails?.first_name} {userLoginDetails?.last_name}</Typography>
             <Typography>{order?.shippingAddress?.address}</Typography>
-            <Typography>{order?.shippingAddress?.city} - {order?.shippingAddress?.zipcode} </Typography> 
+            <Typography>{order?.shippingAddress?.city} - {order?.shippingAddress?.zipcode} </Typography>
             <Typography>{order?.shippingAddress?.state} - {order?.shippingAddress?.country}</Typography>
-             <Typography>Phone:{order?.shippingAddress?.phone_number}</Typography>
+            <Typography>Phone:{order?.shippingAddress?.phone_number}</Typography>
           </Grid>
 
         </Grid>
-        <Button  variant='outlined' fullWidth sx={{mb:4, mt:2,borderColor:'brown', color:'inherit', ":hover":{borderColor:'brown'}}}>Support</Button>
-    </Container>
+        <Button variant='outlined' fullWidth sx={{ mb: 4, mt: 2, borderColor: 'brown', color: 'inherit', ":hover": { borderColor: 'brown' } }}>Support</Button>
+      </Container>
     </motion.div>
   )
 }
